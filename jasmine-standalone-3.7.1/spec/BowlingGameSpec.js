16828cd4b706a710f3de7adc01fd368fdf4064a7
Moki Daniel <mokidnl@gmail.com>
2021-03-25 09:10:35 +0300

    ft(BowlingGameSpec.js) described the tests

---
+++ jasmine-standalone-3.7.1/spec/BowlingGameSpec.js
@@ -0,0 +1,24 @@
+/*--In order to write code that mimics a bowling game, let's start with the simplest concept.
+A player rolls the ball!
+
+For each "turn" in bowling, a player gets two rolls.
+
+We'll cover two broad scenarios that could arise when a player rolls:
+
+Player knocks down impressive pins (a spare or a strike)
+
+Player knocks down 0 pins
+
+Spare: when a player hits all the pins over the course of two rolls (one turn)
+
+Strike: when a player hits all the pins in a single roll --*/
+
+describe("bowling game", function() {
+    it("should properly calculate a gutter game", function() {
+        it("should properly calculate a strike", function() {
+            it("should properly calculate a spare", function() {
+
+            })
+        })
+    })
+})
